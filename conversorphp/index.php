8734ee402a6fec8f52e12967be5014c7284041cb
DarkJ-18 <54506019+DarkJ-18@users.noreply.github.com>
2020-01-26 13:19:13 -0500

    Add files via upload

---
+++ conversorphp/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+<meta charset="utf-8">
+<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+<title>Conversor</title>
+    <script>
+
+      function proceso(txtnumero1,boton){
+        switch(boton){
+          
+            case "PesoCO":
+            var parametros={
+              "txtnumero1":txtnumero1,
+              "btnmulpeso":boton
+            };
+            break;
+            case "PesoUS":
+            var parametros={
+              "txtnumero1":txtnumero1,
+              "btnmuldolar":boton
+            };
+            break;
+
+        }
+
+        $.ajax({
+          data:parametros,
+          url:'Calcular.php',
+          type:'post',
+          beforeSend:
+          function(){
+            $('#resultado').html('cargando!...');
+          },
+          success:
+          function(response){
+            $('#resultado').html(response);
+          }
+        });
+
+      }
+
+
+    </script>
+
+
+</head>
+<body>
+   
+    <form name="form1" method="POST">
+     moneda: <input type="text" name="txtnumero1" id="txtnumero1">
+      <br>
+      <input type="button" name="btnpeso" id="btnpeso" value="PesoCO"
+      onclick="proceso($('#txtnumero1').val(),$('#btnpeso').val());">
+      <br>
+      <input type="button" name="btnpesodolar" id="btnpesodolar" value="PesoUS"
+      onclick="proceso($('#txtnumero1').val(),$('#btnpesodolar').val());">
+     
+    </form>
+    <br>
+    <span id="resultado"></span>
+    <script src="jquery-3.4.1.js"></script>
+</body>
+
+</html>
